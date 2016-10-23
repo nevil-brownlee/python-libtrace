7f7b75d7b54ffd264e5c628599fdd2850781b9be
Nevil Brownlee <n.brownlee@auckland.ac.nz>
2016-10-23 18:25:06 +1300

    Add new example program to test icmp6 payload

--- doc/examples/icmp6.py
+++ doc/examples/icmp6.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python
 
-# Thu, 13 Mar 14 (PDT)
-# icmp6.py: Demonstrate ICMP6 (v6) header decodes
+# Sun, 23 Oct 2016 (NZDT)
+# icmp6.py: Demonstrate ICMP (v4) header decodes
 # Copyright (C) 2015, Nevil Brownlee, U Auckland | WAND
 
 from plt_testing import *
 
 t = get_example_trace('icmp6-sample.pcap')
 
-n = 0;  offset = 12
+n = 0;  nicmp = 0
+offset = 12
 
 for pkt in t:
+    n += 1
+
     icmp6 = pkt.icmp6
     if not icmp6:
         continue
-    n += 1
 
     print "%5d: " % (n),
     print_icmp6(icmp6, offset)
     print
 
+    nicmp += 1
+    #if nicmp == 10:
+    #    break
+
 t.close
+
+print "%d packets examined\n" % (n)
