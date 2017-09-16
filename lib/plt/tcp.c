318603a16ec2d28ea50fec94ce2d0a712c5034ed
Nevil Brownlee <n.brownlee@auckland.ac.nz>
2017-09-16 17:46:52 +1200

    UDP and TCP.payload don't pass self as MOM to the new Data object, since they share the original packet data

--- lib/plt/tcp.c
+++ lib/plt/tcp.c
@@ -336,7 +336,7 @@ static PyObject *tcp_get_payload(DataObject *self, void *closure) {
       }
    Py_INCREF(self);
    DataObject *pld_obj = plt_new_object(&TransportType,
-      RLT_TYPE_L5, RLT_KIND_CPY, NULL, (PyObject *)self,
+      RLT_TYPE_L5, RLT_KIND_CPY, NULL, Py_None,
 				       /* (PyObject *)self,  16 Sep 2017 */
       self->l2p, self->l2_rem, self->linktype, self->ethertype, self->vlan_tag,
       self->l3p, self->l3_rem, 6,  &hp[tcp_len], self->rem-tcp_len);
