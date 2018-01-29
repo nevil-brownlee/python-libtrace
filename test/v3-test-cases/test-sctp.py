0f8741e5c957fbc58ae2bcd32786c604cc7580fe
Nevil Brownlee <n.brownlee@auckland.ac.nz>
2018-01-30 12:12:51 +1300

    SCTP dissection implemented

---
+++ test/v3-test-cases/test-sctp.py
@@ -0,0 +1,37 @@
+# 1123, Tue 30 Jan 2018 (NZDT)
+#
+# test-sctp.py: Test SCTP handling in plt
+#
+# Copyright (C) 2018, Nevil Brownlee, U Auckland | WAND
+
+from plt_testing import *
+
+import plt
+
+tcp_fn = "sctp-demo.pcap"  # Data file name
+
+t = plt.trace("pcapfile:"+tcp_fn)
+t.start()        # Start t
+
+n = 0
+for pkt in t:  # Get next packet
+    n += 1     # Count it
+    sctp = pkt.sctp
+    if not sctp:
+        continue
+    s = "%4d: src_port %u, dst_port %d, vf tag %08x, checksum %08x" % (
+        n, sctp.src_port, sctp.dst_port, sctp.verification_tag, sctp.checksum)
+    test_println(s, get_tag())
+    for cx,ch in enumerate(sctp.chunks):
+        s = "%4d    chunk %d:  type %d, flags %02x length %d bytes OK %s " % (
+            n, cx, ch.type, ch.flags, ch.length, ch.is_ok)
+        test_println(s, get_tag())
+        s = "  "*10
+        for c in ch.bytes[0:16]:
+            s += " %02x" % c
+        test_println(s, get_tag())
+    test_println("", get_tag())
+    
+t.close()
+
+test_println("%d packets examined\n" % n, get_tag())
